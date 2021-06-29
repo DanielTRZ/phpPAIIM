efd9ce9418d852a96fdd1191182d5707dfb6d483
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-06-30 00:01:55 +0200

    Update zaloguj.php

--- sesje/zaloguj.php
+++ sesje/zaloguj.php
@@ -2,18 +2,13 @@
 <html lang="pl">
 
   <head>
-
      <meta charset="utf-8">
      <title>Zaloguj Sesje</title>
-
      <meta name="description" content="Test Sesja">
      <meta name="keywords" content="Sesja">
      <meta name="Sesjon" content="Sesja">
-
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="sesja.css">
-     
-
   </head>
   <body>
   
@@ -25,5 +20,4 @@
       ?>
 
   </body>
-
 </html>
