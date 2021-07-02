ec8b52a62be8d78051011db088f11c4d5ca203c1
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-07-02 19:52:15 +0200

    Update ciasteczkalicznik.php

--- ciasteczka/ciasteczkalicznik.php
+++ ciasteczka/ciasteczkalicznik.php
@@ -2,18 +2,13 @@
 <html lang="pl">
 
   <head>
-
      <meta charset="utf-8">
      <title>Licznik wizyt ciasteczka PHP</title>
-
      <meta name="description" content="Test Ciasteczko">
      <meta name="keywords" content="ciasteczko">
      <meta name="Ciastek" content="Ciasteczkowy Potwór">
-
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="arkusz.css">
-     
-
   </head>
   <body>
    <h1>Licznik twoich odwiedzin na stronie po odświeżeniu</h1>
