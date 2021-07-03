8c54bc2f6bbd5ff098ec4a4a0d85a1b26d1fba20
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-07-03 21:06:19 +0200

    Update stan1.php

--- sesje/stan1.php
+++ sesje/stan1.php
@@ -2,18 +2,13 @@
 <html lang="pl">
 
   <head>
-
      <meta charset="utf-8">
      <title>Licznik wizyt ciasteczka PHP</title>
-
      <meta name="description" content="">
      <meta name="keywords" content="">
      <meta name="" content="">
-
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="sesja.css">
-     
-
   </head>
   <body>
       
@@ -29,5 +24,4 @@
       ?>
 
   </body>
-
 </html>
